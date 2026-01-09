2d09491f1a0a9c221312a84a45833c7498e8ea05
harish.mani <harish.mani@zohocorp.com>
2026-01-09 22:15:57 +0530

    small required changes for code refactring

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -6,7 +6,7 @@
 extern uint8_t __bss_start;
 extern uint8_t __end;
 
-void __attribute__((section(".entry"))) start(uint16_t bootDrive)
+void __attribute__((section(".entry"))) kstart(uint16_t bootDrive)
 {
     memset(&__bss_start, 0, (&__end) - (&__bss_start));
 
