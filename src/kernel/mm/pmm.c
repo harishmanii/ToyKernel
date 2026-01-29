9f7f5d6bb44f61437f8ba1cd7baa05768e105555
harish.mani <harish.mani@zohocorp.com>
2026-01-29 22:54:21 +0530

    added a functionality to allocate the memory in byte and page wise

--- src/kernel/mm/pmm.c
+++ src/kernel/mm/pmm.c
@@ -5,6 +5,7 @@
 static uint32_t TOTAL_MEMORY; // total memory in bytes
 static uint32_t TOTAL_PAGES; // total no of 4kb pages
 static uint32_t TOTAL_BLOCK; // is the number of uint32_t entries in your bitmap.
+static uint32_t TOTAL_USED_BLOCKS = 0;
 
 static uint32_t* BIT_MAP=0;  // Bit map start address
 static uint32_t BITMAP_SIZEB; // total bytes needed to cover the entire memory address
@@ -85,11 +86,16 @@ void reserve_memory_block(uintptr_t base,size_t size)
     uintptr_t start_page = base / PER_BLOCK_SIZE;
     size_t pages = (size + PER_BLOCK_SIZE - 1) / PER_BLOCK_SIZE;
 
+      if(TOTAL_USED_BLOCKS >= TOTAL_BLOCK){
+        return;
+      }
+
     for (uint32_t i = 0; i < pages; i++) {
         // if (page >= MAX_PAGES)
         //     panic("reserve_memory_block: out of range");
         if (!test_bit(start_page + i)) {
             set_bit(start_page + i);
+            TOTAL_USED_BLOCKS++;
         }
     }
 }
@@ -101,6 +107,7 @@ void unreserve_memory_block(uint32_t base,uint32_t size)
 
     for (uint32_t i = 0; i < pages; i++) {
             unset_bit(start_page + i);
+            TOTAL_USED_BLOCKS--;
     }
 }
 
@@ -112,23 +119,94 @@ void reserve_memory()
     reserve_memory_block((uint32_t)BIT_MAP,BITMAP_SIZEB);   //reserve the memory BIT_MAP
 }
 
-uintptr_t allocate_memory()
+uintptr_t allocate_chunk()
 {
     //TODO: Need to track the no of blocks consumed, if no blocks less then 0 return -1 here
     // ASSUMES identity-mapped physical memory
+    if(TOTAL_USED_BLOCKS >= TOTAL_BLOCK){
+      return 0;
+    }
     int32_t frame = get_free_memory_block();
     if (frame == INVALID_FRAME)
     return 0;
     set_bit(frame);
     uintptr_t addr = frame * PER_BLOCK_SIZE;
-    memset((void*)addr,0, PER_BLOCK_SIZE); 
+    TOTAL_USED_BLOCKS++;
+    // memset((void*)addr,0, PER_BLOCK_SIZE); for 
     return addr;
+}
+
+uint32_t get_free_frame_by_size(size_t size)
+{
+  if(size==0){
+    return INVALID_FRAME;
+  }else if(TOTAL_USED_BLOCKS >= TOTAL_BLOCK){
+    return INVALID_FRAME;
+  }
+  else if(size==1){
+    return get_free_memory_block(); //it returns the frame no only
+  }
+
+  for(uint32_t i = 0 ; i < TOTAL_BLOCK ; i++){
+    if(BIT_MAP[i]!=0xffffffff){
+      for(uint32_t j = 0 ;j<32;j++){
+        uint32_t bit = i * 32 + j;
+        // need to work on this
+        uint32_t free = 0;
+        if(!test_bit(bit)){
+          for(uint32_t count = 0;count<=size;count++){
+            if(!test_bit(bit+count)){
+              free++;
+            }
+            else{
+              break;
+            }
+            if(free==size){
+              return bit;
+            } 
+          }
+        }
+      }
+    }
+  }
+  return INVALID_FRAME;
+}
+
+
+uintptr_t allocate_byte(size_t size){
+  uint32_t frames = (size + PER_BLOCK_SIZE - 1) / PER_BLOCK_SIZE; // convert the size to exact no of fram so we can assign a byte
+  uint32_t frame = get_free_frame_by_size(frames);
+  
+  if(frame == INVALID_FRAME)return 0;
+
+  for(uint32_t i = 0; i < frames;i++){
+    set_bit(frame+i);
+    TOTAL_USED_BLOCKS++;
+  }
+  uintptr_t addr = frame*PER_BLOCK_SIZE;
+  memset((void*)addr,0,size);
+  return addr;
+}
+
+uintptr_t allocate_page(size_t size){
+  uint32_t frame = get_free_frame_by_size(size);
+  
+  if(frame == INVALID_FRAME)return 0;
+
+  for(uint32_t i = 0; i < size;i++){
+    set_bit(frame+i);
+    TOTAL_USED_BLOCKS++;
+  }
+  uintptr_t addr = frame*PER_BLOCK_SIZE;
+  memset((void*)addr,0,size*PER_BLOCK_SIZE);
+  return addr;
 }
 
 void deallocate_memory(uintptr_t base)
 {
     uintptr_t frame = base / PER_BLOCK_SIZE;
     unset_bit(frame);
+    TOTAL_USED_BLOCKS--;
 }
 
 int deallocate_memory_by_size(uint32_t base,uint32_t size)
@@ -138,6 +216,7 @@ int deallocate_memory_by_size(uint32_t base,uint32_t size)
 
     for (uint32_t i = 0; i < pages; i++) {
         unset_bit(start + i);
+        TOTAL_USED_BLOCKS--;
     }
 }
 
@@ -148,6 +227,9 @@ void INIT_MEMORY(BootParams* bootParams)
     get_memory_info(&bootParams->Memory);
     setup_memory_bitmap();
     reserve_memory(); 
+
+
+
 }
 
 
