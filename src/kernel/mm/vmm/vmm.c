0f07230f3249c3e5b5dd2e4ff3c410aebe547f83
harish.mani <harish.mani@zohocorp.com>
2026-02-02 21:47:44 +0530

    did some comments and code refactor , need to jmp to higher half

--- src/kernel/mm/vmm/vmm.c
+++ src/kernel/mm/vmm/vmm.c
@@ -4,19 +4,26 @@
 page_directory *current_page_directory = 0;
 
 
-page_directory* setup_page_directory_table()
+//setting up the initial page directory which holds the page directory for the mmu it needed
+// initially it setup with supervisor and read/write mode only without present bit ,if we use the page na we can add some attribute
+// so thats why we allocate a single page directory
+bool setup_page_directory_table()
 {
-    page_directory *dir = (page_directory *)allocate_page(3);
-    if (!dir) return false; // Out of memory
-     memset(dir, 0, sizeof(page_directory));
+    current_page_directory = (page_directory *)allocate_page(1);
+    if (!current_page_directory) return false; // Out of memory
+     memset(current_page_directory, 0, sizeof(page_directory));
 
     for (uint32_t i = 0; i < 1024; i++)
-        dir->entries[i] = 0x02; // Supervisor, read/write, not present
+        current_page_directory->entries[i] = 0x0;
 
-    return dir;
+    return true;
 }
 
-page_table* setup_page_table()
+// for initially we need a single page table for enabling the paging feature after that we can get as we need
+// 1024 is 1024 entries each page table can hold 1024 entries  
+// here we creating page table for setting up the higher half addr
+// here we ,map our kernel physical address with virtual address  
+bool setup_higher_half_kernel()
 {
     page_table *table = (page_table *)allocate_page(1);
     if (!table) return false;   // Out of memory
@@ -24,7 +31,6 @@ page_table* setup_page_table()
     memset(table, 0, sizeof(page_table));
 
     for (uint32_t i = 0, frame = KERNEL_ADDRESS, virt = 0xC0000000; i < 1024; i++, frame += PAGE_SIZE, virt += PAGE_SIZE) {
-        // Create new page
         pt_entry page = 0;
         SET_ATTRIBUTE(&page, PTE_PRESENT);
         SET_ATTRIBUTE(&page, PTE_READ_WRITE);
@@ -33,44 +39,53 @@ page_table* setup_page_table()
         // Add page to 0-4MB page table
         table->entries[PT_INDEX(virt)] = page;
     }
+    // setting up the higher half address with our page we created above with the page_directory entry
+    pd_entry *entry = &current_page_directory->entries[PD_INDEX(0xC0000000)];
+    SET_ATTRIBUTE(entry, PDE_PRESENT);
+    SET_ATTRIBUTE(entry, PDE_READ_WRITE);
+    SET_FRAME(entry, (physical_address)table); // 3GB directory entry points to default page table
 
-    return table;
+    return true;
 }
 
-page_table* setup_higher_half_kernel()
+// here we map this as identity mapping so kernel can jmp to this without page fault error
+// here we just copy a current physical addr as virtual address so when kernel enable the paging it will pass the addr to mmu , mmu will check for the page directory but will not there a entry so it will throws page fault exception
+// here also we map the page table with page directory
+bool setup_page_table()
 {
-    page_table *table3G = (page_table *)allocate_page(1);
+    page_table *table = (page_table *)allocate_page(1);
     
-    if (!table3G) return false;   // Out of memory
+    if (!table) return false;   // Out of memory
 
-    memset(table3G, 0, sizeof(page_table));
+    memset(table, 0, sizeof(page_table));
 
     for (uint32_t i = 0, frame = 0x0, virt = 0x0; i < 1024; i++, frame += PAGE_SIZE, virt += PAGE_SIZE) {
-        // Create new page
         pt_entry page = 0;
         SET_ATTRIBUTE(&page, PTE_PRESENT);
         SET_ATTRIBUTE(&page, PTE_READ_WRITE);
         SET_FRAME(&page, frame);
 
-        // Add page to 3GB page table
-        table3G->entries[PT_INDEX(virt)] = page;
+        table->entries[PT_INDEX(virt)] = page;
     }
+    pd_entry *entry2 = &current_page_directory->entries[PD_INDEX(0x00000000)];
+    SET_ATTRIBUTE(entry2, PDE_PRESENT);
+    SET_ATTRIBUTE(entry2, PDE_READ_WRITE);   
+    SET_FRAME(entry2, (physical_address)table);
 
-    return table3G;
+    return true;
+
 }
 
 bool enable_paging()
 {
- __asm__ __volatile__ ("movl %CR0, %EAX; orl $0x80000001, %EAX; movl %EAX, %CR0");
+    __asm__ __volatile__ ("movl %CR0, %EAX; orl $0x80000001, %EAX; movl %EAX, %CR0");
 
     return true;    
 }
 
-bool set_page_directory(page_directory *pd)
+bool set_page_directory()
 {
-    if (!pd) return false;
-
-    current_page_directory = pd;
+    if (!current_page_directory) return false;
 
     // CR3 (Control register 3) holds address of the current page directory
     __asm__ __volatile__ ("movl %%EAX, %%CR3" : : "a"(current_page_directory) );
@@ -80,27 +95,18 @@ bool set_page_directory(page_directory *pd)
 
 bool INITIALIZE_VMEMORY(void)
 {
-    page_directory *dir = setup_page_directory_table();
+    setup_page_directory_table();
 
-    page_table *table =  setup_page_table();
+    setup_higher_half_kernel();
+
+    setup_page_table();
     
-    page_table *table3G = setup_higher_half_kernel();
+    set_page_directory();
 
-    pd_entry *entry = &dir->entries[PD_INDEX(0xC0000000)];
-    SET_ATTRIBUTE(entry, PDE_PRESENT);
-    SET_ATTRIBUTE(entry, PDE_READ_WRITE);
-    SET_FRAME(entry, (physical_address)table); // 3GB directory entry points to default page table
-
-    pd_entry *entry2 = &dir->entries[PD_INDEX(0x00000000)];
-    SET_ATTRIBUTE(entry2, PDE_PRESENT);
-    SET_ATTRIBUTE(entry2, PDE_READ_WRITE);   
-    SET_FRAME(entry2, (physical_address)table3G);    // Default dir entry points to 3GB page table
-
-    // Switch to page directory
-    set_page_directory(dir);
-
+    // after enabled the paging we are still in the identity mapping , need to move to higher half 
     enable_paging();
 
+    return true
 }
 
 
